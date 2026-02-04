98fc2a835cd6d219892f8f59f1310d4c75f6a3ca
adrienpineau <pineau.adrien@hotmail.com>
2026-02-04 09:50:04 +0100

    Init SpringBoot BookHub BackEnd

---
+++ src/test/java/fr/bookHub/BookHubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bookHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
