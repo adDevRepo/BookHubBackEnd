6ebb6990038aab34919414d6661fa64a72002aaa
adrienpineau <pineau.adrien@hotmail.com>
2026-02-05 22:48:12 +0100

    ajout des repos avis et categorie , mis en place du package et du fichier de test testAvisRepo mais a terminer car il manque les autres repos

---
+++ src/test/java/fr/bookHub/dal/AvisRepositoryTest.java
@@ -0,0 +1,22 @@
+package fr.bookHub.dal;
+
+import fr.bookHub.bo.Avis;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AvisRepositoryTest {
+
+    private AvisRepository avisRepository;
+
+    void findByLivreIdAndUtilisateurId() {
+
+        // ajout utilisateur
+
+        // ajout livre
+
+        // ajout avis
+        Avis avis = new Avis(
+
+        );
+    }
+}
